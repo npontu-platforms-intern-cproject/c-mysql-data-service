8600341659caee8fe954e791b97281d56d831e34
EJAyarma <johnayarmahe@gmail.com>
2021-10-29 10:45:08 +0000

    setup

--- main.c
+++ main.c
@@ -1,32 +1,10 @@
-#include <mysql.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <mysql.h>
 
 int main(int argc, char **argv)
 {
-    MYSQL *con = mysql_init(NULL);
+  printf("MySQL client version: %s\n", mysql_get_client_info());
 
-    if (con == NULL)
-    {
-        fprintf(stderr, "%s\n", mysql_error(con));
-        exit(1);
-    }
-
-    if (mysql_real_connect(con, "localhost", "root", "",
-                           NULL, 0, NULL, 0) == NULL)
-    {
-        fprintf(stderr, "%s\n", mysql_error(con));
-        mysql_close(con);
-        exit(1);
-    }
-
-    if (mysql_query(con, "CREATE DATABASE testdb"))
-    {
-        fprintf(stderr, "%s\n", mysql_error(con));
-        mysql_close(con);
-        exit(1);
-    }
-
-    mysql_close(con);
-    exit(0);
+  exit(0);
 }
